2969ecd9063779658179ae978b472ae2628a58e9
Wong Tze Hsiung <wotzhs@gmail.com>
2016-06-21 00:20:20 +0800

    updated on JS to dynamically change its color at different section

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+	
+	$(document).scroll(function(){
+		var section1 = $("#profile").offset().top;
+		var section2 = $("#portfolio").offset().top;
+		var section3 = $("#contact").offset().top;
 
+		if($(document).scrollTop() >= section1 && $(document).scrollTop() < (section2-100)){
+			$('.navbar').css("background-color", "rgba(0,0,0,0)")
+		}else if($(document).scrollTop() >= (section2-100) && $(document).scrollTop() < section3){
+			$('.navbar').css("background-color", "rgba(1,1,1,1)")
+		}else if($(document).scrollTop() >= section3){
+			$('.navbar').css("background-color", "rgba(255,165,0,1)")
+		}
+	})
 
     $(window).scroll(function() {
 		if ($(document).scrollTop() > 350) { 
